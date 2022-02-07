d7c09a72a64fc54e3c53c8455e9065933dabfd67
jayhawker6 <andrewgguernsey@gmail.com>
2022-02-07 11:57:42 -0600

    Added even more!
    Signed-off-by: jayhawker6 <andrewgguernsey@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-from easygui import *
 import json
+
+from easygui import *
 
 
 # Define Classes
@@ -16,28 +16,56 @@ def ohgod():  # Let's... Move on.
     msgbox("Let's just move on.", title, "Yeah...")
 
 
+def addwtff(x=1):
+    global wtff
+    oldwtff = wtff
+    wtff += x
+    tempstr = "WTF Factor: " + str(oldwtff) + " --> " + str(wtff)
+    msgbox(tempstr, "The Mild West", "...What the...")
+
+
 # Load Data
 f = open('json.json')
 data = json.load(f)
 
 # Initialize some vars
 title = "The Mild West"
-buttons = ["Milk", "Slaughter", "Castrate"]
-pressed = buttonbox(data['1'], title, buttons)
-if pressed == "Milk":
-    msgbox(json["3"], title, "What?")  # But something is VERY wrong
-    pressed = buttonbox("This is a bull.", title, ["I know what I'm doing!", "What do you mean?", "(Ignore Narrator)"])
+strength = 1
+stupidity = 1
+wtff = 1
 
-    if pressed == "I know what I'm doing!":  # I Know what i'm doing!
-        msgbox("Okay, Weirdo. Just saying... That's DISGUSTING.", title, "Shut up and tell the story.")  # Weirdo.
-        msgbox("You drink that milk out of that udder and wait... It's real milk? What?", title, "Always Has Been.")
+# Begin the Bull
+while True:
+    buttons = ["Milk", "Slaughter", "Castrate"]
+    pressed = buttonbox(data["1"], title, buttons)
+    if pressed == "Milk":  # Chose to milk the bull
+        msgbox(data["3"], title, "What")  # But something is VERY wrong
+        buttons = ["I know what I'm doing!", "What do you mean?", "Ignore Narrator"]
+        pressed = buttonbox("This is a bull.", title, buttons)
 
-    elif pressed == "What do you mean?":  # What do you mean?
-        msgbox(json["2"], title, "So what am I milking?")  # What am I milking?
-        pressed = buttonbox("I think you know EXACTLY what you are \"Milking,\" Pardner.", title,
-                            ["Oh god...", "No really i'm clueless what is it?"])
+        if pressed == "I know what I'm doing!" or pressed == "Ignore Narrator":  # I Know what i'm doing!
+            msgbox("Okay, Weirdo. Just saying... That's DISGUSTING.", title, "Shut up and tell the story.")  # Weirdo.
+            msgbox("You drink that milk out of that udder and wait... It's real milk? What!?", title,
+                   "Always Has Been...")
+            addwtff(2)
+            msgbox("How the hell did you do that? Still Disgusting but I applaud. Anyways, Time to move on.", title,
+                   "Trudge Onward!")
+            break
 
-        if not pressed == "Oh god...":  # You know what's being milked
-            msgbox("The only thing to \"Milk\" down there on a MALE COW is... It's weiner.", title, "Oh god...")
-        ohgod()
+        elif pressed == "What do you mean?":  # What do you mean?
+            msgbox(data["2"], title, "So what am I milking?")  # What am I milking?
+            pressed = buttonbox("I think you know EXACTLY what you are \"Milking,\" Pardner.", title,
+                                ["Oh god...", "No really i'm clueless what is it?"])
+            msgbox(data["4"], title, "I'm sorry!")
+            ohgod()
+            msgbox("Anyways, Time to move on.", title, "But i'm still hungry!")
+            break
 
+    elif pressed == "Castrate":
+        pressed = buttonbox("What in tarnation gave you the urge to choose that option!", title,
+                            ["*Shrug*", "Nevermind..."])
+        if pressed == "*Shrug*":
+            pass
+        else:
+            msgbox("Yeah don't you touch that poor thing! What did it ever do to you, you monster!", title, "I guess")
+            break
