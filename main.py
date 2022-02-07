ee091fec239411a61d52281977cea06ddf90b6ee
jayhawker6 <andrewgguernsey@gmail.com>
2022-02-07 10:56:43 -0600

    Changed framework, added some options.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
+from easygui import *
+import json
 
+
+# Define Classes
 
 class FatalSystemError(Exception):
     def __init__(self, message="Give the class a string to work with you dummy or it defaults to this!"):
@@ -7,60 +11,33 @@ class FatalSystemError(Exception):
         super().__init__(self.message)
 
 
-def keycontinue():
-    os.system("pause")
+# Define Functions
+def ohgod():  # Let's... Move on.
+    msgbox("Let's just move on.", title, "Yeah...")
 
 
-# noinspection SpellCheckingInspection
-errorlist = tuple([
-    "Well crap. The house burned down!",
-    "OH GOD THE LEECHES THEY'RE IN MY EYeEeeEieIEIEIEIEeesss..."
-])
+# Load Data
+f = open('json.json')
+data = json.load(f)
 
-print("Welcome to the python programming hellscape!")
-print("Here you will endure the painful story of coding a python program.")
-print("Enjoy!")
-keycontinue()
+# Initialize some vars
+title = "The Mild West"
+buttons = ["Milk", "Slaughter", "Castrate"]
+pressed = buttonbox(data['1'], title, buttons)
+if pressed == "Milk":
+    msgbox(json["3"], title, "What?")  # But something is VERY wrong
+    pressed = buttonbox("This is a bull.", title, ["I know what I'm doing!", "What do you mean?", "(Ignore Narrator)"])
 
-while True:
-    a = input("You have unimagineable piles of homework. What shall you do? >>|[ ")
-    if a.lower() == "burn" or a.lower() == "burn it!":
-        a = input("Are you ABSOLUTELY SURE that you want to burn it? This might not be the best place to do it! "
-                  ">>|[ ")
-        if a.lower() == "yes":
-            print("kk")
-            raise FatalSystemError(errorlist[0])
-    elif a.lower() == "finish":
-        print("Nope.")
-    elif a.lower() == "kill":
-        print("You grab a knife from the kitchen and stab it straight through the paper, over and over again.")
-        keycontinue()
-        print("Unfortunately the stacks of paper do not respond in any way. You go put the knife back.")
-        keycontinue()
-    elif a.lower() == "throw":
-        print("You pick some of it up and throw, as it goes a little ways but slowly floats back down to the floor.")
-        keycontinue()
-    elif a.lower() == "bleed":
-        print("You pick up the pile of paper. Time to go on a field trip!")
-        keycontinue()
-        print("You take it to the beach. As you wade into the water you grab leeches because... idk why'd you expect "
-              "a good story?")
-        keycontinue()
-        print("Anyways you have some leeches and now that you've grabbed a few you head back over to that godforsaken "
-              "stack of paper. It had this coming...")
-        keycontinue()
-        print("You pour the leeches onto that evil pile of disgusting useless schoolwork. The leeches latch onto the "
-              "paper...")
-        keycontinue()
-        print("And they then decide that the paper has no blood in it so they head for you.")
-        keycontinue()
-        raise FatalSystemError(errorlist[1])
-    elif a.lower() == "torture":
-        print("You head over the the stretcher you have in the corner for the neibour's kid's. It will serve a "
-              "different purpose this time!")
-        keycontinue()
-        print("The paper goes onto the two ropes, you pull and... The paper tears. It lets out no moans or wails of "
-              "agony, as alas, you were just defeated by an inanimate object. Oh the humanity!!!")
-        keycontinue()
-    else:
-        raise SystemExit
+    if pressed == "I know what I'm doing!":  # I Know what i'm doing!
+        msgbox("Okay, Weirdo. Just saying... That's DISGUSTING.", title, "Shut up and tell the story.")  # Weirdo.
+        msgbox("You drink that milk out of that udder and wait... It's real milk? What?", title, "Always Has Been.")
+
+    elif pressed == "What do you mean?":  # What do you mean?
+        msgbox(json["2"], title, "So what am I milking?")  # What am I milking?
+        pressed = buttonbox("I think you know EXACTLY what you are \"Milking,\" Pardner.", title,
+                            ["Oh god...", "No really i'm clueless what is it?"])
+
+        if not pressed == "Oh god...":  # You know what's being milked
+            msgbox("The only thing to \"Milk\" down there on a MALE COW is... It's weiner.", title, "Oh god...")
+        ohgod()
+
